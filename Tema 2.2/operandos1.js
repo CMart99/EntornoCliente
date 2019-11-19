7d8fd2d6fd07ec3f66153838651f247f5bfdfd86
CMart99 <45295748+CMart99@users.noreply.github.com>
2019-11-19 11:27:22 +0100

    Corregido operandos1.js

--- Tema 2.2/operandos1.js
+++ Tema 2.2/operandos1.js
@@ -3,134 +3,113 @@
 // DEBE TOCARSE EL CÓDIGO ANTES DEL RETURN PARA QUE LA FUNCIÓN DEVUELVA TRUE
 
 function asignacionDeValoresSumar(){
- 
-    let x = 2;
-    let y = 1;
- 
-     x = x + y;
- 
+
+    let x = 0;
+    let y = 0;
+
+    x +=3;
+
     return x === 3;
 }
- 
- 
+
+
 function asignacionDeValoresRestar(){
- 
-    let x = 10;
-    let y = 5;
- 
-    x = x - y;
- 
+
+    let x = 0;
+    let y = 0;
+    x -=-5;
+
     return x === 5;
 }
- 
+
 function asignacionDeValoresMultiplicacion(){
- 
+
     let x = 5;
-    let y = 7;
- 
-    x = x * y;
- 
+    let y = 0;
+    x *=7;
     return x === 35;
 }
- 
+
 function asignacionDeValoresResto(){
- 
-    let x = 1;
-    let y = 10;
- 
+
+    let x = 3;
+    let y = 0;
+    x%=2;
     return x === 1;
 }
- 
+
 function comparacionIgualdad(){
- 
-    var x = null;
-    var y = undefined;
-    var x = undefined;
-    var y = null;
-    
-    resultado = x === undefined
-                && y  === null;
-           
-    return resultado;
-}
- 
-function comparacionIgualdadEstricto(){
- 
+
     let x = undefined;
     let y = null;
-    
-    resultado = x === undefined
-                && y  === null;
- 
+    let resultado = x==y;
     return resultado;
 }
- 
+
+function comparacionIgualdadEstricto(){
+
+    let x = undefined;
+    let y = undefined;
+    let resultado = x===y;
+    return resultado;
+}
+
 function compararcionMayorIgual(){
- 
-    let x = 4;
-    let y = 3;
- 
-    resultado = typeof x == "number"
-           && typeof y == "number"
-           && x > y;
-           
+
+    let x = 0;
+    let y = 0;
+    let resultado = x>=y;
     return resultado;
 }
- 
- 
+
+
 function incremento(){
- 
-    let x = 4;
- 
-    ++x * 2;
- 
-    return x === 5;
+
+    let x = 7;
+    x++;
+    return x === 8;
 }
- 
- 
+
+
 function decremento(){
- 
-    let x = 6;
- 
-    --x * 2;
- 
-    return x === 5;
+
+    let x = 3;
+    x--;
+    return x === 2;
 }
- 
- 
+
+
 function igualdadIncremento(){
- 
-    let x = 4;
- 
-    x++ * 2;
- 
-    return x++ === 5;
+
+    let x = 8;
+
+    return x++ === 8;
 }
- 
+
 function comparacionAND(){
     var x = "hola";
     var y = "hola";
-    
-    resultado = x === "hola"
-    && y  === "hola";
- 
+    if(y && x ==="hola"){
+         resultado = true;
+    }
+
     return resultado;
 }
- 
+
 function comparacionOR(){
     var x = "hola";
-    var y = "hola";
-    
-    resultado = x === "hola"
-    || y  === "hola";
- 
+    var y = "hola2";
+    if(y || x ==="hola"){
+        resultado = true;
+   }
     return resultado;
 }
- 
+
 function comparacionNOT(){
-    
-    var x = !true              
-    var resultado = !false             
- 
+    var x = "hola";
+    if(x !== "hola2"){
+        resultado=true;
+    }
+
     return resultado;
 }
