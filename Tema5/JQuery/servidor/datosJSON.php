c00b9e7aea506f9122beffa984a0561df251e588
CMart99 <45295748+CMart99@users.noreply.github.com>
2020-02-02 17:44:40 +0100

    Peticiones JQuery

---
+++ Tema5/JQuery/servidor/datosJSON.php
@@ -0,0 +1,26 @@
+<?php
+
+sleep(1);
+
+header('Content-Type: application/json');
+header('Cache-Control: no-cache, must-revalidate');
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$db = "distroada";
+
+$conn = mysqli_connect($server, $user, $pass, $db);
+
+$query = "SELECT * FROM distribuciones";
+
+$queryResult = mysqli_query($conn, $query);
+
+$result = array();
+
+while ($col = mysqli_fetch_array($queryResult)) {
+	$result[] = $col;
+}
+echo json_encode($result);
+
+mysqli_close($conn);
